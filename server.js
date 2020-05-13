fc66dc0d3db6bffacb948f5c142649956d014259
Glitch (twitter-bott) <none>
2020-05-13 16:51:05 +0000

    😺⛪️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-/* Setting things up. */
 const express = require( 'express' ),
       app = express(),   
       Twit = require( 'twit' ),
       config = {
-      /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
         twitter: {
           consumer_key: process.env.TWITTER_CONSUMER_KEY,
           consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -15,11 +13,11 @@ const express = require( 'express' ),
 
 app.use( express.static( 'public' ) );
 
-/* You can use cron-job.org, uptimerobot.com, or a similar site to periodically visit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
+
 
 app.all( `/${process.env.BOT_ENDPOINT}`, function( req, res ){
 
-  /* The example below tweets out "Hello world!". */
+  
   
   T.post( 'statuses/update', { status: 'You are stuck in the 20th century if you are still tweeting from an iphone' }, function( err, data, response ) {
     if ( err ){
